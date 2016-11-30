47889576b9b39a2d99193bc24b039754205ee9a2
Kejee <K@zhangdembp.lan>
2016-11-30 09:46:44 +0800

    Initial Commit

---
+++ slider_RGB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  slider_RGB
+//
+//  Created by Keje on 2016/11/30.
+//  Copyright © 2016年 Keje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ slider_RGB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  slider_RGB
+//
+//  Created by Keje on 2016/11/30.
+//  Copyright © 2016年 Keje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
